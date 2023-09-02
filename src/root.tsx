af5e284ab9e52b8bd751466f7c11617e639676d0
falvarador <alvaguez1990@gmail.com>
2023-09-02 05:13:18 -0600

    [fix]: remove error bounday for now

--- src/root.tsx
+++ src/root.tsx
@@ -6,14 +6,12 @@ import type {
   LoaderFunction,
 } from "@remix-run/node";
 import {
-  isRouteErrorResponse,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useRouteError,
 } from "@remix-run/react";
 
 import { ClerkApp, V2_ClerkErrorBoundary } from "@clerk/remix";
@@ -69,32 +67,32 @@ function App() {
   );
 }
 
-function CustomErrorBoundary() {
-  const error = useRouteError();
+// function CustomErrorBoundary() {
+//   const error = useRouteError();
 
-  if (isRouteErrorResponse(error)) {
-    return (
-      <div>
-        <h1>
-          {error.status} {error.statusText}
-        </h1>
-        <p>{error.data}</p>
-      </div>
-    );
-  } else if (error instanceof Error) {
-    return (
-      <div>
-        <h1>Error</h1>
-        <p>{error.message}</p>
-        <p>The stack trace is:</p>
-        <pre>{error.stack}</pre>
-      </div>
-    );
-  } else {
-    return <h1>Unknown Error</h1>;
-  }
-}
+//   if (isRouteErrorResponse(error)) {
+//     return (
+//       <div>
+//         <h1>
+//           {error.status} {error.statusText}
+//         </h1>
+//         <p>{error.data}</p>
+//       </div>
+//     );
+//   } else if (error instanceof Error) {
+//     return (
+//       <div>
+//         <h1>Error</h1>
+//         <p>{error.message}</p>
+//         <p>The stack trace is:</p>
+//         <pre>{error.stack}</pre>
+//       </div>
+//     );
+//   } else {
+//     return <h1>Unknown Error</h1>;
+//   }
+// }
 
-export const ErrorBoundary = V2_ClerkErrorBoundary(CustomErrorBoundary);
+export const ErrorBoundary = V2_ClerkErrorBoundary();
 
 export default ClerkApp(App);

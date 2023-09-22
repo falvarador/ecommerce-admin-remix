e645972e12de13b659c9665544f01f691cb98ea9
falvarador <alvaguez1990@gmail.com>
2023-09-22 09:28:20 -0600

    [feat]: new route added

---
+++ src/routes/_dashboard.$storeId.settings.tsx
@@ -0,0 +1,7 @@
+export default function SettingPage() {
+  return (
+    <>
+      <div>This is the settings!</div>
+    </>
+  );
+}
--- src/routes/_dashboard.$storeId.tsx
+++ src/routes/_dashboard.$storeId.tsx
@@ -1,4 +1,4 @@
-export default function Dashboard() {
+export default function DashboardPage() {
   return (
     <>
       <div>This is the dashboard!</div>
